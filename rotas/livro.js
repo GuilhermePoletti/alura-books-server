920b33e9f0a77562ce97c80d0b9c671e68258f48
Guilherme Poletti <48832384+GuilhermePoletti@users.noreply.github.com>
2023-06-12 08:21:41 -0300

    Initial commit

---
+++ rotas/livro.js
@@ -0,0 +1,18 @@
+const {Router} = require("express")
+const {getLivros, getLivro, postLivro, patchLivro} = require("../controladores/livros")
+
+const router = Router()
+
+router.get('/', getLivros )
+
+router.get('/:id', getLivro )
+
+router.post('/', postLivro)
+
+router.patch('/:id', patchLivro )
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
+})
+
+module.exports = router
---
+++ servicos/livro.js
@@ -0,0 +1,38 @@
+const fs = require("fs")
+
+function getTodosLivros() {
+    return JSON.parse( fs.readFileSync("livros.json"))
+}
+
+function getLivroPorId(id) {
+    const livros = JSON.parse(fs.readFileSync("livros.json"))
+
+    const livroFiltrado = livros.filter(livro => livro.id === id)[0]
+    return livroFiltrado
+}
+
+function insereLivro(livroNovo) {
+    const livros = JSON.parse(fs.readFileSync("livros.json"))
+
+    const novaListaDeLivros = [...livros, livroNovo]
+
+    fs.writeFileSync("livros.json", Json.stringify(novaListaDeLivros))
+}
+
+function modificaLivro(modificacoes, id) {
+    let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
+    const indiceModificado = livrosAtuais.findIndex(livro => livro.id === id)
+
+    const consteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes }
+
+    livrosAtuais[indiceModificado] = consteudoMudado
+
+    fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
+}
+
+module.exports = {
+    getTodosLivros,
+    getLivroPorId,
+    insereLivro,
+    modificaLivro
+}
